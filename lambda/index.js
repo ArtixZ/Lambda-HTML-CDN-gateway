1faae968c51b80f76aa734275476ba5817514c75
Dongyue <artixzhu@gmail.com>
2019-07-07 21:22:19 -0400

    finish

--- lambda/index.js
+++ lambda/index.js
@@ -1,22 +1,67 @@
-'use strict'
-const request = require("request")
+const request = require("request-promise");
+const minify = require("html-minifier").minify;
 
-exports.handler = function (event, context, callback) {
-    console.log(event.path)
 
-    const URL = "http://generative-labs.com/v1" + event.path;
-    console.log("URL:", URL)
-    request(URL, function(err, res) {
-        if(err) {
-            const error = new Error(err)
-            callback(error)
-        }
-        console.log(res)
-    })
-
-    // Lambda Code Here
-    // context.succeed('Success!')
-    // context.fail('Failed!')
-
-    return "Sample message"
+function flatQueryString(str) {
+    return Object.keys(str).map(function(key) {
+        return key + '=' + str[key];
+    }).join('&');
 }
+
+function transferHTML(doc) {
+    const html = `
+        <html>
+        <head>
+        <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0 user-scalable=0"
+        />
+        <meta charset="UTF-8" />
+        <link href="https://s3.amazonaws.com/generativelabs/resouces/bikan_style.css" rel="stylesheet">
+        </head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-135107879-1&l=gDataLayer"></script>
+        <script>
+            window.gDataLayer = window.gDataLayer || [];
+            function gtag(){gDataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'UA-135107879-1');
+        </script>
+        <body>
+        ${doc}
+        </body>
+        <script src="https://s3.amazonaws.com/generativelabs/resouces/bikan.js"></script>
+        </html>
+        `;
+    return minify(html, {
+        collapseWhitespace: true
+    });
+}
+
+
+
+exports.handler = async(event, context, callback) => {
+    // TODO implement
+    console.log("query string: ", event.queryStringParameters);
+    const URL = "http://generative-labs.com/v1" + event.path + "?" + flatQueryString(event.queryStringParameters);
+    console.log("URL:", URL);
+    try {
+        const res = await request(URL);
+        console.log(unescape(res))
+        const response = {
+            statusCode: 200,
+            headers: {"content-type": "text/html"},
+            body: transferHTML(unescape(res)),
+        };
+        return response;
+
+    }
+    catch (err) {
+        const error = new Error(err);
+        callback(error);
+
+        return {
+            statusCode: 300,
+            body: JSON.stringify(err),
+        };
+    }
+};
